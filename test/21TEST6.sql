cbc119ec22c183d936c22aef2e4681dbdc89cb42
camelie_1985@yahoo.com <camelie_1985@yahoo.com@0cffcf25-8dfb-1362-7ca8-9c4a5199799f>
2012-06-21 23:04:08 +0000

    query 6

---
+++ test/21TEST6.sql
@@ -0,0 +1,14 @@
+SELECT ST_AREA(ST_INTERSECTION(m1.geom, m2.geom))
+FROM markup m1, markup m2, (SELECT c1.doc_uid AS d1, c2.doc_uid AS d2
+			    FROM collection c1, collection c2
+			    WHERE c1.pais_uid='IMG1'
+			    AND c2.pais_uid='IMG2'
+			    AND c1.alg_uid='ALG1'
+			    AND c2.alg_uid='ALG1'
+			    AND c1.param_uid='PAR1'
+			    AND c2.param_uid='PAR2') S4
+WHERE m1.doc_uid=S4.d1
+      AND m2.doc_uid=S4.d2
+      AND m1.tile_name='TIL1'
+      AND m2.tile_name='TIL2'
+      AND ST_INTERSECTS(m1.geom,m2.geom)=1; 
